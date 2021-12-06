ae79c0073f3a17d0420d8d2357b887d4c1728f5c
Jesse Woods <jessehwoods@gmail.com>
2021-12-06 15:03:04 -0500

    [Implement] Made skeleton statline.

--- TravellerCharacterGenerator/src/character/Statline.java
+++ TravellerCharacterGenerator/src/character/Statline.java
@@ -1,5 +1,32 @@
 package character;
 
+/**
+ * Used to hold the attributes of the character and return them as either numbers or
+ * the "pseudo-hex" used in Classic Traveller.
+ * 
+ * @author Jesse Woods
+ *
+ */
+
 public class Statline {
 
+	private int strength;
+	
+	private int dexterity;
+	
+	private int endurance;
+	
+	private int intelligence;
+	
+	private int education;
+	
+	private int social;
+	
+	/**
+	 * Returns the attributes in the "pseudo-hexadecimal" used in Classic Traveller.
+	 */
+	public String toString() {
+		//TODO
+	}
+	
 }
